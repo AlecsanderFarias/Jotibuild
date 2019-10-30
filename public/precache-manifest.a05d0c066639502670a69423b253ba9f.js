804814f9be3c1daa839374c9d498b0e570123f87
alecs <souzakissi@gmail.com>
2019-10-30 08:16:40 -0300

    build

---
+++ public/precache-manifest.a05d0c066639502670a69423b253ba9f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "438d93261d2b89ff08729331e2f2a819",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8c5cb34099a9081b0003",
+    "url": "/static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "8c5cb34099a9081b0003",
+    "url": "/static/js/2.c6c504f6.chunk.js"
+  },
+  {
+    "revision": "b37d04507c3224501460",
+    "url": "/static/js/main.915bbe80.chunk.js"
+  },
+  {
+    "revision": "2ea0958070c9d0d8b7df",
+    "url": "/static/js/runtime-main.592df91f.js"
+  }
+]);
