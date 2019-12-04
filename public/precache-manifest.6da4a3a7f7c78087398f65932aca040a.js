091c225fd05ad1b2e3a9310dfbcb01d09c6d2108
Alecsander Farias <alecs@devfarias.com>
2019-12-04 16:30:58 -0300

    coloring tasks by status

--- public/precache-manifest.6da4a3a7f7c78087398f65932aca040a.js
+++ public/precache-manifest.6da4a3a7f7c78087398f65932aca040a.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "8e32f70d5ca4ed53b8f129d7ab8661a5",
+    "revision": "809c376677ef137034e5b83a414691f7",
     "url": "/index.html"
   },
   {
-    "revision": "d18aa88791c5e5e87aa8",
+    "revision": "87508448ac7fec14ce48",
     "url": "/static/css/2.5dbdccff.chunk.css"
   },
   {
-    "revision": "d18aa88791c5e5e87aa8",
-    "url": "/static/js/2.f7880945.chunk.js"
+    "revision": "87508448ac7fec14ce48",
+    "url": "/static/js/2.3944661d.chunk.js"
   },
   {
-    "revision": "38badf8bf1d158b57b6d",
-    "url": "/static/js/main.aee8a4ca.chunk.js"
+    "revision": "3cd9035304e72ee5f087",
+    "url": "/static/js/main.7919ac6a.chunk.js"
   },
   {
     "revision": "2ea0958070c9d0d8b7df",
--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d749d43df11088c934992136d3a93811.js"
+  "/precache-manifest.6da4a3a7f7c78087398f65932aca040a.js"
 );
 
 self.addEventListener('message', (event) => {
